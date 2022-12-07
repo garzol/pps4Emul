c1fa3ff42e4ce7efb93fb7e4677dd6cbd467a8c0
garzol <dubophil@free.fr>
2022-12-07 22:27:36 +0100

    c'est vraiment naze ce git a la con

---
+++ pps4/A17IO.py
@@ -0,0 +1,17 @@
+'''
+Created on 6 déc. 2022
+
+@author: garzol
+'''
+
+class A17IO(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
